1f7feb13c4af1f44ed4e70d295fb2e9dc3163c43
Justin S <jrscottie@hotmail.com>
2022-09-09 13:12:27 +1000

    Deployed Justin

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Justin <code>src/App.js</code> and save to reload.
+          Justin <code>src/App.js</code> testing.
         </p>
         <a
           className="App-link"
